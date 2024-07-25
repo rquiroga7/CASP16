f3c8acbbc5691b850c0cd84f0aff58d51c0d3cba
rquiroga7 <rquiroga@unc.edu.ar>
2024-07-25 15:47:23 -0300

    changes

--- L1000_results/get_energies.sh
+++ L1000_results/get_energies.sh
@@ -1,16 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
 for i in L1001 L1002 L1003 L1004 L1005 L1006 L1007 L1008 L1009 L1010 L1011 L1012 L1013 L1014 L1015 L1016; do
     echo "" >> energies.txt
     echo -n $i >> energies.txt
